b38e5237aaa16450ad83ad5de69fdf2df2c68b43
Gabriel <murariugabi23@gmail.com>
2023-10-23 13:23:41 +0300

    Installed project and defined some objectives

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link, Route, Routes } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // Here I will need to define my paths
+
+  // you have to make 3 or 4 pages which one has to be login and nav and footer should not be accessible
+
+  // remember that you want the footer andd nav to show when all pages are accessed
+
+
+  
+  // you need to document yourself of how to write your css when yu work in react because last react project has a lot of css code that was overwritten multiple times without use
+
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>hello</h1>
+    </div>
   )
 }
 
